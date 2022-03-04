42103dca604066b4c0ce8a3c57c705f5336a76f7
YRollHid <yans.roll@gmail.com>
2022-03-04 14:26:22 +0100

    Update acr-build-push.sh script

--- scripts/acr-build-push.sh
+++ scripts/acr-build-push.sh
@@ -3,7 +3,8 @@ ACR_NAME=${ACRNAME}
 RESOURCE_GROUP=${AKSRG}
 
 # Build Services images
-ROOTPATHSERVICES='~/src/Services'
+ROOTPATHSERVICES='src/Services'
+BROOTPATHSERVICES='../../'
 
 # Build & Push Basket-API
 cd $ROOTPATHSERVICES/Basket/Basket.API
@@ -13,6 +14,7 @@ az acr build \
     --image basket-api:${TAG} .
 
 # Build & Push Catalog-API
+cd $BROOTPATHSERVICES
 cd $ROOTPATHSERVICES/Catalog/Catalog.API
 az acr build \
     --resource-group $RESOURCE_GROUP \

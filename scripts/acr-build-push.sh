85e74ce6c7a64f3b17112ce635ac8724846f45f6
YRollHid <yans.roll@gmail.com>
2022-03-04 14:05:37 +0100

    Add SRC & CI - pipeline

---
+++ scripts/acr-build-push.sh
@@ -0,0 +1,25 @@
+# Set variables
+ACR_NAME=${ACRNAME}
+RESOURCE_GROUP=${AKSRG}
+
+# Build Services images
+ROOTPATHSERVICES='~src/Services'
+
+# Build & Push Basket-API
+cd $ROOTPATHSERVICES/Basket/Basket.API
+az acr build \
+    --resource-group $RESOURCE_GROUP \
+    --registry $ACR_NAME \
+    --image basket-api:${TAG} .
+
+# Build & Push Catalog-API
+cd $ROOTPATHSERVICES/Catalog/Catalog.API
+az acr build \
+    --resource-group $RESOURCE_GROUP \
+    --registry $ACR_NAME \
+    --image catalog-api:${TAG} .
+
+# Verify the images
+az acr repository list \
+    --name $ACR_NAME \
+    --output table
